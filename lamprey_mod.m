3d2329f69b3b7a76e9e0a42a139c31735cf6f02b
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2024-10-08 14:01:48 -0700

    initial upload

---
+++ lamprey_mod.m
@@ -0,0 +1,50 @@
+function dydt = lamprey_mod(t,y,params, I1, I2)
+%% Set variable names
+V     = y(1); % factor V
+Va    = y(2); % factor Va
+X     = y(3); % factor X
+Xa    = y(4); % factor Xa
+P     = y(5); % prothrombin
+T     = y(6); % thrombin
+
+%% Set parameter names
+% NOTE: update updatepars.m to get parameter list from set_params()
+kF = params(1);
+V_up = params(2);
+k5 = params(3);
+kT5 = params(4);
+kT10 = params(5);
+X_up = params(6);
+P_up = params(7);
+TF_VIIa0 = params(8);
+
+%% Model equations
+dydt = zeros(length(y), 1);
+
+
+% Algebraic
+a5  = k5 * T; %functional form of a5([T])
+
+a10 = TF_VIIa0; % functional form of a10([TF:VIIa]_0)
+
+aT  = kT5*Va + kT10*Xa; % functional form of aT([Xa],[Va])
+
+% ODES
+% d(V)/dt
+dydt(1) = kF*(V_up - V) - a5*V;
+
+% d(Va)/dt
+dydt(2) = a5*V - kF*Va;
+
+% d(X)/dt
+dydt(3) = kF*(X_up - X) - a10*X;
+
+% d(Xa)/dt 
+dydt(4) = a10*X - I1*Xa - kF*Xa;
+
+% d(P)/dt
+dydt(5) = kF*(P_up - P) - aT*P;
+
+% d(T)/dt
+dydt(6) = aT*P - I2*T - kF*T;
+end
---
+++ set_params.m
@@ -0,0 +1,23 @@
+function p = set_params()
+
+% flow
+p.kF = 0; %0.1; % flow
+
+% Factor V
+p.V_up = 1; 
+p.k5 = 1;
+p.kT5 = 0.1;
+
+% Factor Va
+p.kT10 = 0.1;
+
+% Factor X
+p.X_up = 1;
+
+
+% Prothrombin
+p.P_up = 1;
+
+% [TF:VIIa]0
+p.TF_VIIa0 = 1;
+end
